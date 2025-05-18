e0f5c288e5b0b3bb2bb62ab7eb8c5e555b041788
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-05-18 13:18:42 +0530

    all set

--- back-end/server.js
+++ back-end/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://[::1]:5173'],
+  origin: ['http://localhost:5173','https://khata-book-lime.vercel.app/', 'http://[::1]:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
